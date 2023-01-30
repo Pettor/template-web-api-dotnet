4132dbfbc1fb55614a76bf028a18b1f584eb20c3
Petter Hancock <pettor@gmail.com>
2023-01-30 16:13:42 +0100

    Fix for logged no longer needed

--- src/Infrastructure/Common/Services/EventPublisher.cs
+++ src/Infrastructure/Common/Services/EventPublisher.cs
@@ -7,16 +7,15 @@ namespace Backend.Infrastructure.Common.Services;
 
 public class EventPublisher : IEventPublisher
 {
-    private readonly ILogger<EventPublisher> _logger;
     private readonly IPublisher _mediator;
 
-    public EventPublisher(ILogger<EventPublisher> logger, IPublisher mediator) =>
-        (_logger, _mediator) = (logger, mediator);
+    public EventPublisher(IPublisher mediator) =>
+        _mediator = mediator;
 
     public Task PublishAsync(IEvent @event)
     {
         var eventName = @event?.GetType().Name;
-        LoggerMessage.Define(LogLevel.Information, new EventId(1, eventName), $"Publishing Event : {eventName}");
+        LoggerMessage.Define<EventPublisher>(LogLevel.Information, new EventId(1, eventName), $"Publishing Event : {eventName}");
         return _mediator.Publish(CreateEventNotification(@event!));
     }
 
