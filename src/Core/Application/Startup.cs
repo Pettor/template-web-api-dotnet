b1b640353620a8fcac1b8f4bee55a1e5ddfe05f0
Petter Hancock <pettor@gmail.com>
2023-11-06 22:45:08 +0100

    fix: formatting in startup.cs

--- src/Core/Application/Startup.cs
+++ src/Core/Application/Startup.cs
@@ -5,11 +5,11 @@ namespace Backend.Application;
 
 public static class Startup
 {
-	public static IServiceCollection AddApplication(this IServiceCollection services)
-	{
-		var assembly = Assembly.GetExecutingAssembly();
-		return services
-			.AddValidatorsFromAssembly(assembly)
-			.AddMediatR(assembly);
-	}
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        return services
+            .AddValidatorsFromAssembly(assembly)
+            .AddMediatR(assembly);
+    }
 }
